cf92296dfd940c6081e376635a9c1aedf657c775
Akbar <akbardhia19@gmail.com>
2025-02-15 20:28:13 +0700

    Making Home Fragment Ui And Fix Bugs

--- app/src/main/java/com/example/managerusaha/MainActivity.kt
+++ app/src/main/java/com/example/managerusaha/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.managerusaha
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.graphics.Color
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mainlayout)
+        actionBar?.hide()
+        window.statusBarColor = ContextCompat.getColor(this, R.color.primary)
+        window.navigationBarColor = ContextCompat.getColor(this, R.color.white)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.nav_view)
+
+        // set home
+        if (savedInstanceState == null) {
+            replaceFragment(HomeFragment())
+        }
+
+        // navbar Handle
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> replaceFragment(HomeFragment())
+
+            }
+            true
+        }
+    }
+
+    // Func fragment
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
+
