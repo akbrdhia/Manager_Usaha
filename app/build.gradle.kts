91bbeb30d819304d4e60cdad7f7900bd2d8dc153
Akbar <akbardhia19@gmail.com>
2025-06-02 22:12:06 +0700

    Chore : update minimal sdk from 24 -> 28

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
     defaultConfig {
         applicationId = "com.managerusaha.app"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
